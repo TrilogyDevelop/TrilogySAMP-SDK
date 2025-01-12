33d495de7786856c01ce9e1b82664af461d3c9e4
why ega <egaaaaaaaaaaaaaaaaa@mail.ru>
2025-01-12 17:26:43 +0500

    add network methods

--- include/trilogysamp-sdk/network.hpp
+++ include/trilogysamp-sdk/network.hpp
@@ -22,6 +22,21 @@ class INetwork {
  public:
   [[nodiscard]] virtual EventDispatcher<INetworkEventHandler>&
   GetEventDispatcher() = 0;
+  virtual bool Connect(std::string host, std::uint16_t port,
+                       std::string password, bool is_used_proxy) = 0;
+  virtual void Disconnect() = 0;
+  [[nodiscard]] virtual bool IsConnected() const = 0;
+  virtual void SetName(std::string name) = 0;
+  [[nodiscard]] virtual std::string GetName() const = 0;
+  virtual void SetPassword(std::string password) = 0;
+  [[nodiscard]] virtual std::string GetPassword() const = 0;
+  [[nodiscard]] virtual std::string GetServerAddress() const = 0;
+  [[nodiscard]] virtual std::uint16_t GetServerPort() const = 0;
+  [[nodiscard]] virtual std::uint32_t GetPing() const = 0;
+  virtual void SetFakePing(std::uint32_t ping) = 0;
+  virtual void SetVersion(std::string version) = 0;
+  virtual void SetProxy(std::string host, std::uint16_t port) = 0;
+  [[nodiscard]] virtual bool IsUseProxy() const = 0;
   [[nodiscard]] virtual RakClientInterface& GetRakClientInterface() = 0;
   [[nodiscard]] virtual IPlayerPool& GetPlayerPool() = 0;
   [[nodiscard]] virtual IVehiclePool& GetVehiclePool() = 0;
